e2a409f01cd5392868fb216fea06e9dfd80e8a1f
Szymon <osx@osxs-iMac.local>
2023-03-22 21:46:42 +0100

    fisrt

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class intochordsapp {
+    static ArrayList<String> noteslist = new ArrayList<>();
+    static String[] notesarray = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" , "A", "B" ,"H"};
+    public static void main(String[] args) {
+        for (int i = 0; i < noteslist.size(); i++) {
+            System.out.println(noteslist.get(i));
+        }
+    }
+}
