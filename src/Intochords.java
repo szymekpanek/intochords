3c91661528601be42968bae1e778eb1af641a0fb
qocureq <makskocur@gmail.com>
2023-03-23 15:22:57 +0100

    created generator for notes board

--- src/Intochords.java
+++ src/Intochords.java
@@ -1,10 +1,34 @@
 package src;
+import java.util.Random;
 
 public class Intochords {
+    static Random random = new Random();
     static String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" , "A", "B" ,"H"};
     public static void main(String[] args) {
-        for (String note : notes) {
-            System.out.println(note);
+        String[][] notesBoard = new String[4][4];
+
+        int noteIndex = random.nextInt(notes.length);
+
+        for (int i = 0; i < notesBoard.length; i++) {
+            for (int j = 0; j < notesBoard[i].length; j++) {
+                if (i == j) {
+                    if (notes[noteIndex].length() == 1) {
+                        notesBoard[i][j] = notes[noteIndex] + " ";
+                    } else {
+                        notesBoard[i][j] = notes[noteIndex];
+                    }
+                } else {
+                    notesBoard[i][j] = "__";
+                }
+                notesBoard[i][j] += " ";
+            }
+        }
+
+        for (String[] column : notesBoard) {
+            for (String row : column) {
+                System.out.print(row);
+            }
+            System.out.println();
         }
     }
 }
