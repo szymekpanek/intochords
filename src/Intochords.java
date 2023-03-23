28da4f4ba208a3e89c448c9a413913797c28be99
qocureq <makskocur@gmail.com>
2023-03-23 11:46:41 +0100

    refactor

---
+++ src/Intochords.java
@@ -0,0 +1,8 @@
+public class Intochords {
+    static String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" , "A", "B" ,"H"};
+    public static void main(String[] args) {
+        for (String note : notes) {
+            System.out.println(note);
+        }
+    }
+}
