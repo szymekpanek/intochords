75fcab4a393d3c23512f79fc54322a26d8af3a85
qocureq <makskocur@gmail.com>
2023-03-23 12:22:13 +0100

    cleanup

--- src/Intochords.java
+++ src/Intochords.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Intochords {
     static String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" , "A", "B" ,"H"};
     public static void main(String[] args) {
