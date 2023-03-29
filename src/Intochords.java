de438a4c16399af3030ff3469bf16e6213cd216e
szymekpanek <szympanek2002@gmail.com>
2023-03-29 12:18:46 +0200

    grammar fix

--- src/Intochords.java
+++ src/Intochords.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 
 public class Intochords {
 
-    static String[] noteslist = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B", "H"};
-
+    static String[] notesarray = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B", "H"};
 
     private static void printboard(String[][] board) {
         for (String[] column : board) {
@@ -20,14 +19,14 @@ public class Intochords {
     private static void Board(String[][] board) {
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
-        int randomNote = random.nextInt(noteslist.length);
+        int randomNote = random.nextInt(notesarray.length);
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (i == j) {
-                    if (noteslist[randomNote].length() == 1) {
-                        board[i][j] = noteslist[randomNote] + " ";
+                    if (notesarray[randomNote].length() == 1) {
+                        board[i][j] = notesarray[randomNote] + " ";
                     } else {
-                        board[i][j] = noteslist[randomNote];
+                        board[i][j] = notesarray[randomNote];
                     }
                 } else {
                     board[i][j] = "__";
@@ -43,7 +42,7 @@ public class Intochords {
         boolean first_check, second_check;
         System.out.println("Podaj dzwięk [1][2]: ");
         String first_answer = scan.nextLine();
-        if (first_answer.equals(noteslist[(randomNote + 4) % 12])) {
+        if (first_answer.equals(notesarray[(randomNote + 4) % 12])) {
             first_check = true;
         } else {
             first_check = false;
@@ -52,14 +51,14 @@ public class Intochords {
         System.out.println("Podaj dzwięk [1][3]: ");
 
         String second_answer = scan.nextLine();
-        if (second_answer.equals(noteslist[(randomNote + 7) % 12])) {
+        if (second_answer.equals(notesarray[(randomNote + 7) % 12])) {
             second_check = true;
         } else {
             second_check = false;
         }
 
         if (first_check && second_check) {
-            System.out.println("Gratulacje akord " + noteslist[randomNote] + "," + first_answer + "," + second_answer + "\nczyli " + noteslist[randomNote] +" dur \n");
+            System.out.println("Gratulacje akord " + notesarray[randomNote] + "," + first_answer + "," + second_answer + "\nczyli " + notesarray[randomNote] +" dur \n");
         } else System.out.println("Podane dzwięki nie tworza akordu!");
     }
 
